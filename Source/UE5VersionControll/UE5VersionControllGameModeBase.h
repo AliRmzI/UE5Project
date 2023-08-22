0ddec5a93330609aa3e0f957d568f0c831b995d6
Ali RMZ <ali327713.ar@gmail.com>
2023-08-22 15:24:48 +0330

    Created Unreal Engine 5 Project

---
+++ Source/UE5VersionControll/UE5VersionControllGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5VersionControllGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5VERSIONCONTROLL_API AUE5VersionControllGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
